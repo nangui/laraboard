8beabb7313c16d603114254dd7f4078e2ef244b7
Adonaï Elohim Exaucé <adonainangui03@gmail.com>
2019-04-04 19:33:53 +0000

    Index projects page styled

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,7 +25,15 @@
             <div class="container mx-auto">
                 <div class="flex justify-between items-center py-2">
                     <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
+                        <svg xmlns="http://www.w3.org/2000/svg" width="140" height="39" viewBox="0 0 140 39">
+                            <g id="Groupe_3" data-name="Groupe 3" transform="translate(-280 -230)">
+                              <g id="Groupe_2" data-name="Groupe 2" transform="translate(271 226)">
+                                <path id="Tracé_2" data-name="Tracé 2" d="M12.454,37,9,39.784l6.6.852L12.3,43,26,40.636" fill="none" fill-rule="evenodd"/>
+                                <path id="Tracé_3" data-name="Tracé 3" d="M42.273,4C27.487,4,15.326,15.078,14.037,29.157A87.371,87.371,0,0,1,23.26,18.8a.738.738,0,0,1,1.029-.01.7.7,0,0,1,.01,1q-1.359,1.351-2.688,2.732l-.175.184C16.793,27.552,13.474,31.767,11.064,37a.7.7,0,0,0,.365.937.74.74,0,0,0,.963-.356,38.585,38.585,0,0,1,2.974-5.273A28.438,28.438,0,0,0,39.618,17.794a.7.7,0,0,0-.016-.7.737.737,0,0,0-.625-.344H36.283l4.83-2.689a.714.714,0,0,0,.328-.384A26.88,26.88,0,0,0,43,4.708.718.718,0,0,0,42.273,4Z" fill="#47d5fa" fill-rule="evenodd"/>
+                              </g>
+                              <text id="Laraboard" transform="translate(320 261)" fill="#551a8b" stroke="#551a8b" stroke-linecap="round" stroke-linejoin="round" stroke-width="1" font-size="22" font-family="SegoeUI, Segoe UI"><tspan x="0" y="0">Laraboard</tspan></text>
+                            </g>
+                        </svg>
                     </a>
 
                     <div>
--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,18 +1,27 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="flex items-center mb-3">
-        <a href="/projects/create">New project</a>
-    </div>
+    <header class="flex items-center mb-3 py-4">
+        <div class="flex justify-between items-end w-full">
+            <h2 class="text-grey text-sm font-normal">My Projects</h2>
+            <a href="/projects/create" class="button">New project</a>
+        </div>
+    </header>
 
-    <div class="flex">
+    <main class="lg:flex lg:flex-wrap -mx-3">
         @forelse ($projects as $project)
-            <div class="bg-white mr-4 p-5 rounded shadow w-1/3" style="height: 200px;">
-                <h3 class="font-normal text-xl py-6">{{ $project->title }}</h3>
-                <div class="text-grey">{{ str_limit($project->description, 100) }}</div>
+            <div class="lg:w-1/3 px-3 pb-6">
+                <div class="bg-white p-5 rounded-lg shadow" style="height: 200px;">
+                    <h3 class="font-normal text-xl py-6 -ml-5 border-l-4 border-blue-light mb-4 pl-4">
+                        <a href="{{ $project->path() }}" class="text-black no-underline">
+                            {{ $project->title }}
+                        </a>
+                    </h3>
+                    <div class="text-grey">{{ str_limit($project->description, 100) }}</div>
+                </div>
             </div>
         @empty
             <p>No projects yet.</p>
         @endforelse
-    </div>
+        </main>
 @endsection
